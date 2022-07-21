5166d349e8fd14e2ec3e3cb8bc788bd04736288d
Luigipzz <luigipozzi.it@gmail.com>
2022-07-21 12:39:35 +0200

    Enable Cors

--- VideogamesCollection/src/main/java/com/videogamescollections/lp/controller/api/VideogameController.java
+++ VideogamesCollection/src/main/java/com/videogamescollections/lp/controller/api/VideogameController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,6 +28,7 @@ public class VideogameController {
 		
 	}
 	
+	@CrossOrigin
 	@GetMapping(value = "/api/videogames")
 	public Iterable<Videogame> getAll(){
 		return videogameService.getAll();
